85e6b22168e294d64d3507e9ce2e752236fa5f37
kehiy <kehiiiiya@gmail.com>
2024-09-25 21:18:49 +0330

    fix(server, databae): nip01.

--- database/config.go
+++ database/config.go
@@ -3,6 +3,6 @@ package database
 type Config struct {
 	URI               string
 	DBName            string `yaml:"db_name"`
-	ConnectionTimeout int32  `yaml:"connection_timeout_in_ms"`
-	QueryTimeout      int32  `yaml:"query_timeout_in_ms"`
+	ConnectionTimeout int16  `yaml:"connection_timeout_in_ms"`
+	QueryTimeout      int16  `yaml:"query_timeout_in_ms"`
 }
