5211ec76eed5d85152f9158282e4c979c565ba5f
ZigBalthazar <zigbalthazar@gmail.com>
2024-09-26 16:50:21 +0330

    fix: change request

--- config/parameters.go
+++ config/parameters.go
@@ -7,6 +7,7 @@ import (
 	"github.com/dezh-tech/immortal"
 	"github.com/dezh-tech/immortal/database"
 	"github.com/dezh-tech/immortal/handler"
+	"github.com/dezh-tech/immortal/server"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -28,6 +29,7 @@ type Subscription struct {
 }
 type Parameters struct {
 	Handler        *handler.Config `bson:"handler"         json:"handler"`
+	Server         *server.Config  `bson:"handler"         json:"server"`
 	Retention      *Retention      `bson:"retention"       json:"retention"`
 	Fees           *Subscription   `bson:"fees"            json:"fees"`
 	Name           string          `bson:"name"            json:"name"`
@@ -69,22 +71,26 @@ func (c *Config) LoadParameters(db *database.Database) error {
 			PostingPolicy:  "",                                                                 // posting policy URL
 			PaymentsURL:    "",                                                                 // payments URL
 			Icon:           "",                                                                 // icon URL
+			Server: &server.Config{
+				Limitation: &server.Limitation{
+					MaxMessageLength: 8192,  // Maximum length of a single message (in bytes or characters)
+					MaxSubscriptions: 20,    // Maximum number of concurrent subscriptions a client can create
+					MaxFilters:       20,    // Maximum number of filters a client can apply in a subscription
+					MaxSubidLength:   256,   // Maximum length of a subscription identifier
+					MinPowDifficulty: 0,     // Minimum proof-of-work difficulty for publishing events
+					AuthRequired:     false, // Whether authentication is required for writes
+					PaymentRequired:  false, // Whether payment is required to interact with the relay
+					RestrictedWrites: false, // Whether writes are restricted to authenticated or paying users
+				},
+			},
 			Handler: &handler.Config{
 				InitialQueryDefaultLimit: 100,
 				Limitation: handler.Limitation{
-					MaxMessageLength:    8192,  // Maximum length of a single message (in bytes or characters)
-					MaxSubscriptions:    20,    // Maximum number of concurrent subscriptions a client can create
-					MaxFilters:          20,    // Maximum number of filters a client can apply in a subscription
-					MaxLimit:            1000,  // Maximum number of events returned in a query
-					MaxSubidLength:      256,   // Maximum length of a subscription identifier
-					MaxEventTags:        200,   // Maximum number of tags allowed in a single event
-					MaxContentLength:    4096,  // Maximum content length of an event (in bytes)
-					MinPowDifficulty:    0,     // Minimum proof-of-work difficulty for publishing events
-					AuthRequired:        false, // Whether authentication is required for writes
-					PaymentRequired:     false, // Whether payment is required to interact with the relay
-					RestrictedWrites:    false, // Whether writes are restricted to authenticated or paying users
-					CreatedAtLowerLimit: 0,     // Earliest timestamp allowed for event creation
-					CreatedAtUpperLimit: 0,     // Latest timestamp allowed for event creation (0 for no limit)
+					MaxLimit:            1000, // Maximum number of events returned in a query
+					MaxEventTags:        200,  // Maximum number of tags allowed in a single event
+					MaxContentLength:    4096, // Maximum content length of an event (in bytes)
+					CreatedAtLowerLimit: 0,    // Earliest timestamp allowed for event creation
+					CreatedAtUpperLimit: 0,    // Latest timestamp allowed for event creation (0 for no limit)
 				},
 			},
 		}
--- handler/config.go
+++ handler/config.go
@@ -1,19 +1,11 @@
 package handler
 
 type Limitation struct {
-	MaxMessageLength    int  `bson:"max_message_length"     json:"max_message_length"`
-	MaxSubscriptions    int  `bson:"max_subscriptions"      json:"max_subscriptions"`
-	MaxFilters          int  `bson:"max_filters"            json:"max_filters"`
-	MaxLimit            int  `bson:"max_limit"              json:"max_limit"`
-	MaxSubidLength      int  `bson:"max_subid_length"       json:"max_subid_length"`
-	MaxEventTags        int  `bson:"max_event_tags"         json:"max_event_tags"`
-	MaxContentLength    int  `bson:"max_content_length"     json:"max_content_length"`
-	MinPowDifficulty    int  `bson:"min_pow_difficulty"     json:"min_pow_difficulty"`
-	AuthRequired        bool `bson:"auth_required"          json:"auth_required"`
-	PaymentRequired     bool `bson:"payment_required"       json:"payment_required"`
-	RestrictedWrites    bool `bson:"restricted_writes"      json:"restricted_writes"`
-	CreatedAtLowerLimit int  `bson:"created_at_lower_limit" json:"created_at_lower_limit"`
-	CreatedAtUpperLimit int  `bson:"created_at_upper_limit" json:"created_at_upper_limit"`
+	MaxLimit            int `bson:"max_limit"              json:"max_limit"`
+	MaxEventTags        int `bson:"max_event_tags"         json:"max_event_tags"`
+	MaxContentLength    int `bson:"max_content_length"     json:"max_content_length"`
+	CreatedAtLowerLimit int `bson:"created_at_lower_limit" json:"created_at_lower_limit"`
+	CreatedAtUpperLimit int `bson:"created_at_upper_limit" json:"created_at_upper_limit"`
 }
 
 type Config struct {
