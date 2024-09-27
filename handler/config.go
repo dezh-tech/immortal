53fe2b11b9862da0a54aa543a775fff370092b49
kehiy <kehiiiiya@gmail.com>
2024-09-27 14:23:42 +0330

    fix errors.

--- handler/config.go
+++ handler/config.go
@@ -1,11 +1,15 @@
 package handler
 
 type Limitation struct {
-	MaxLimit            uint16 `bson:"max_limit"              json:"max_limit"`
-	MaxEventTags        uint   `bson:"max_event_tags"         json:"max_event_tags"`         // todo(@zig)::: move to server.
-	MaxContentLength    uint   `bson:"max_content_length"     json:"max_content_length"`     // todo(@zig)::: move to server.
-	CreatedAtLowerLimit uint   `bson:"created_at_lower_limit" json:"created_at_lower_limit"` // todo(@zig)::: move to server.
-	CreatedAtUpperLimit uint   `bson:"created_at_upper_limit" json:"created_at_upper_limit"` // todo(@zig)::: move to server.
+	MaxLimit uint16 `bson:"max_limit"              json:"max_limit"`
+	// todo(@zig)::: move to server.
+	MaxEventTags uint `bson:"max_event_tags"         json:"max_event_tags"`
+	// todo(@zig)::: move to server.
+	MaxContentLength uint `bson:"max_content_length"     json:"max_content_length"`
+	// todo(@zig)::: move to server.
+	CreatedAtLowerLimit uint `bson:"created_at_lower_limit" json:"created_at_lower_limit"`
+	// todo(@zig)::: move to server.
+	CreatedAtUpperLimit uint `bson:"created_at_upper_limit" json:"created_at_upper_limit"`
 }
 
 type Config struct {
