294ef857af43fb6b0276deeda765998f0967758f
kehiy <kehiiiiya@gmail.com>
2024-09-27 14:29:34 +0330

    fix errors.

--- handler/config.go
+++ handler/config.go
@@ -1,14 +1,11 @@
 package handler
 
 type Limitation struct {
-	MaxLimit uint16 `bson:"max_limit"              json:"max_limit"`
+	MaxLimit uint16          `bson:"max_limit"              json:"max_limit"`
 	// todo(@zig)::: move to server.
-	MaxEventTags uint `bson:"max_event_tags"         json:"max_event_tags"`
-	// todo(@zig)::: move to server.
-	MaxContentLength uint `bson:"max_content_length"     json:"max_content_length"`
-	// todo(@zig)::: move to server.
+	MaxEventTags        uint `bson:"max_event_tags"         json:"max_event_tags"`
+	MaxContentLength    uint `bson:"max_content_length"     json:"max_content_length"`
 	CreatedAtLowerLimit uint `bson:"created_at_lower_limit" json:"created_at_lower_limit"`
-	// todo(@zig)::: move to server.
 	CreatedAtUpperLimit uint `bson:"created_at_upper_limit" json:"created_at_upper_limit"`
 }
 
