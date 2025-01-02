36fa22873d5234ccec583693ef6f35008ac0dc28
Kay <kehiiiiya@gmail.com>
2025-01-02 15:51:12 +0000

    refactor(nip-01): accepting 1 filter per request.

--- server/websocket/event_handler.go
+++ server/websocket/event_handler.go
@@ -280,8 +280,9 @@ func (s *Server) handleEvent(conn *websocket.Conn, m message.Message) { //nolint
 
 			return
 		}
-		_ = conn.WriteMessage(1, message.MakeOK(true, msg.Event.ID, ""))
 	}
+	
+	_ = conn.WriteMessage(1, message.MakeOK(true, msg.Event.ID, ""))
 
 	_, err = s.redis.Client.BFAdd(qCtx, s.redis.BloomFilterName, eID[:]).Result()
 	if err != nil {
