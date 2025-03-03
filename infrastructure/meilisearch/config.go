d23feb8b0b387e68f14e9558b848ad0903fa0b80
sadq <mamsa0205@gmail.com>
2025-03-03 10:44:05 +0330

    chore(config): update port datatype setting

--- infrastructure/meilisearch/config.go
+++ infrastructure/meilisearch/config.go
@@ -2,7 +2,7 @@ package meilisearch
 
 type Config struct {
 	Host              string `yaml:"host"`
-	Port              int16  `yaml:"port"`
+	Port              int    `yaml:"port"`
 	Timeout           int16  `yaml:"timeout_in_ms"`
 	DefaultCollection string `yaml:"default_collection"`
 }
