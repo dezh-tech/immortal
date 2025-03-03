6bd5d3c6d7adcf31b579c8837fce24b4bd1d082c
sadq <mamsa0205@gmail.com>
2025-03-03 11:36:53 +0330

    chore(config): change port datatype from int to int16

--- infrastructure/meilisearch/config.go
+++ infrastructure/meilisearch/config.go
@@ -2,7 +2,7 @@ package meilisearch
 
 type Config struct {
 	Host              string `yaml:"host"`
-	Port              int    `yaml:"port"`
+	Port              uint16 `yaml:"port"`
 	Timeout           int16  `yaml:"timeout_in_ms"`
 	DefaultCollection string `yaml:"default_collection"`
 	APIKey            string
