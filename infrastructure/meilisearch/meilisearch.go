a933bf2e7279f4c71254f6c1e7db4135a311f71a
sadq <mamsa0205@gmail.com>
2025-03-03 10:52:43 +0330

    fix(infrastructure): add api key for meilisearch client

--- infrastructure/meilisearch/meilisearch.go
+++ infrastructure/meilisearch/meilisearch.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/meilisearch/meilisearch-go"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -17,8 +18,11 @@ func New(cfg Config) *Meili {
 	httpClient := &http.Client{
 		Timeout: time.Duration(cfg.Timeout) * time.Millisecond,
 	}
+	meiliAPIKey := os.Getenv("MEILI_API_KEY")
+
 	meiliClient := meilisearch.New(fmt.Sprintf("%s:%d", cfg.Host, cfg.Port),
-		meilisearch.WithCustomClient(httpClient))
+		meilisearch.WithCustomClient(httpClient),
+		meilisearch.WithAPIKey(meiliAPIKey))
 
 	return &Meili{
 		Client:            meiliClient,
