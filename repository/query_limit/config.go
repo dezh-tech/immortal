22871725cd7bdad8f1a19e85b0a49ff526d8adc1
sadq <mamsa0205@gmail.com>
2025-03-19 23:46:43 +0330

    refactor: move repository/config.go to new package to avoid import loops and add mutex and getters/setters

---
+++ repository/query_limit/config.go
@@ -0,0 +1,38 @@
+package query_limit
+
+import "sync"
+
+type Config struct {
+	mu sync.RWMutex
+
+	defaultQueryLimit uint32
+	maxQueryLimit     uint32
+}
+
+// GetDefaultQueryLimit safely retrieves the DefaultQueryLimit
+func (c *Config) GetDefaultQueryLimit() uint32 {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.defaultQueryLimit
+}
+
+// SetDefaultQueryLimit safely sets the DefaultQueryLimit
+func (c *Config) SetDefaultQueryLimit(limit uint32) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.defaultQueryLimit = limit
+}
+
+// GetMaxQueryLimit safely retrieves the MaxQueryLimit
+func (c *Config) GetMaxQueryLimit() uint32 {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.maxQueryLimit
+}
+
+// SetMaxQueryLimit safely sets the MaxQueryLimit
+func (c *Config) SetMaxQueryLimit(limit uint32) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.maxQueryLimit = limit
+}
