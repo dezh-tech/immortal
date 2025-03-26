ba0ac5071ff247dc82f849025dc8703ab5e3601c
sadq <mamsa0205@gmail.com>
2025-03-26 19:02:25 +0330

    feat(grpc): add paramsServer and a new method for updating parameters

---
+++ delivery/grpc/params.go
@@ -0,0 +1,21 @@
+package grpc
+
+import (
+	"context"
+	mpb "github.com/dezh-tech/immortal/delivery/grpc/gen"
+)
+
+type paramsServer struct {
+	*Server
+}
+
+func newParamsServer(s *Server) *paramsServer {
+	return &paramsServer{
+		Server: s,
+	}
+}
+
+func (s *paramsServer) UpdateParameters(ctx context.Context, newParams *mpb.UpdateParametersRequest) (*mpb.UpdateParametersResponse, error) {
+	err := s.keeper.LoadParameters(newParams)
+	return &mpb.UpdateParametersResponse{Success: err == nil}, err
+}
