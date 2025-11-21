cb8ac4979f14431464e539a954570adb1ad4147b
Keyhan Alizadeh <me@kehiy.net>
2025-11-21 22:14:45 +0000

    refactor: rewrite in rust (#140)

--- infrastructure/database/config.go
+++
@@ -1,8 +0,0 @@
-package database
-
-type Config struct {
-	URI               string
-	DBName            string `yaml:"db_name"`
-	ConnectionTimeout int16  `yaml:"connection_timeout_in_ms"`
-	QueryTimeout      int16  `yaml:"query_timeout_in_ms"`
-}
--- infrastructure/grpc_client/config.go
+++
@@ -1,7 +0,0 @@
-package grpcclient
-
-type Config struct {
-	Endpoint  string `yaml:"endpoint"`
-	Region    string `yaml:"region"`
-	Heartbeat uint32 `yaml:"heartbeat_in_second"`
-}
