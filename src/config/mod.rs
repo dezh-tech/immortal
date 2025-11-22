96952aa9bdeabd2ca58e486fd6ce6536c5917645
kehiy <me@kehiy.net>
2025-11-22 19:28:25 +0330

    fix: clippy

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -2,7 +2,6 @@ pub mod types;
 
 use crate::config::types::Config;
 use std::fs;
-use toml;
 
 pub fn load(path: &str) -> Result<Config, Box<dyn std::error::Error>> {
     let s = fs::read_to_string(path)?;
